a413664224ec70e614e084f338751d27bf992ba9
Dorothea Treitz <treitz@doctronic.de>
2023-11-12 10:49:28 +0100

    Algorithmus abgewandelt: probierte Pfade werden verlängert
    Im Unterschied zu vorher werden Pfade, die innerhalb der Pfadlängenbegrenzung
    nicht zum Ziel geführt haben, nicht mehr verworfen sondern in der nächsten
    Iteration einfach verlängert. Dabei werden Pfade die denselben Endpunkt
    haben zusammengefasst auf einen einzigen Pfad.
    
    Zusätzlich wurde die Ausgabe des Ergebnisses verbessert und gleicht nun
    im Aufbau den Eingangsdateien. Um den Fortschritt sichtbar zu machen
    wurde das optionale Argument "-v" eingeführt, das während der Ausführung
    ausgibt, welche Pfadlängenbegrenzung gerade gilt.

--- Aufgabe_3_Zauberschule/zauberschule/src/zauberschule/core.clj
+++ Aufgabe_3_Zauberschule/zauberschule/src/zauberschule/core.clj
@@ -2,15 +2,16 @@
   (:require
    [zauberschule.file-reader :as reader]
    [zauberschule.pathfinder :as pathfinder]
-   [zauberschule.file-writer :as writer]))
+   [zauberschule.file-writer :as writer])
+  (:gen-class))
 
 (defn -main
   [& args]
-  (let [file (str "resources/zauberschule" (first args) ".txt")
+  (let [file (last args)
+        options {:print-progress? (= "-v" (first args))}
         blueprint (reader/read-blueprint file)
-        solution (pathfinder/optimize-path blueprint)]
-    #_(writer/write-blueprint blueprint solution)
-    solution))
+        solution (pathfinder/optimize-path blueprint options)]
+    (writer/write-blueprint blueprint solution)))
 ;; Benutzung:
 ;; (-main 0)
 ;; (-main 1)
--- Aufgabe_3_Zauberschule/zauberschule/src/zauberschule/file_writer.clj
+++ Aufgabe_3_Zauberschule/zauberschule/src/zauberschule/file_writer.clj
@@ -8,17 +8,18 @@
 (defn write-blueprint
   [blueprint solution]
   (let [updated-blueprint (merge blueprint solution)
-        num-floors (reduce max (map first (keys blueprint)))
-        num-rows (reduce max (map second (keys blueprint)))
-        num-columns (reduce max (map last (keys blueprint)))]
-    (->> (for [i (range (inc num-floors))
-               j (range (inc num-rows))
-               k (range (inc num-columns))]
+        num-floors (inc (reduce max (map first (keys blueprint))))
+        num-rows (inc (reduce max (map second (keys blueprint))))
+        num-columns (inc (reduce max (map last (keys blueprint))))]
+    (println num-rows num-columns)
+    (->> (for [i (range num-floors)
+               j (range num-rows)
+               k (range num-columns)]
            (get updated-blueprint (list i j k)))
-;         (partition num-columns)
-;         (str/join "")
-;         (partition num-rows)
-;         (str/join "\n")
-;         (partition num-floors)
-;         (str/join "\n")
-         )))
+         (partition num-columns)
+         (map (partial str/join ""))
+         (partition num-rows)
+         (map (partial str/join "\n"))
+         (str/join "\n\n")
+         (println))))
+
